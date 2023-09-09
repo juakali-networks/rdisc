0e4526627fc75f7f3cb88ff5cca241e9649ad602
Peter Kabiri <peter.kabiri@gmail.com>
2023-09-09 20:03:27 +0200

    Improved on subprocess docker commands

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+
 
 class router_discovery():
 
@@ -24,39 +26,53 @@ class router_discovery():
     def step_1(self):
         self.kill_docker_instances()
  
-        print "Starting Docker containers"
-        self.__cmd = "docker run --name rdisc_source -itd rdisc_image sh"
-        self.__docker_id1 = subprocess.check_output(self.__cmd)
-        print("self.__docker_id1")
+        print("Starting Docker containers")
+        test = subprocess.check_output("pwd")
+        #         self.__docker_id1 = subprocess.check_output(self.__cmd.format(self.__system_vlan_interface, self.__ip1, mod_id).split()).strip()
+        self.__cmd = "docker run --name rdisc_src -itd rdisc_image sh"
+        self.__docker_id1 = subprocess.check_output(self.__cmd.format().split()).strip()
+
         print(self.__docker_id1)
 
 
-        self.__cmd = "docker run --name rdisc_destination -itd rdisc_image sh"
-       #  self.__docker_id2 = subprocess.check_output(self.__cmd).split()).strip()
-        self.__docker_id2 = subprocess.check_output(self.__cmd)
+        self.__cmd = "docker run --name rdisc_dst -itd rdisc_image sh"
 
-        print("self.__docker_id2")
+        self.__docker_id2 = subprocess.check_output(self.__cmd.format().split()).strip()
 
-        self.__cmd = "docker exec -it rdisc_source sh"
-        self.__docker_id3 = subprocess.check_output(self.__cmd)
-        print("self.__docker_id3")
 
-        self.__cmd = "docker exec -it rdisc_destination sh"
-        self.__docker_id3 = subprocess.check_output(self.__cmd)
-        print("self.__docker_id4")
+        print(self.__docker_id2)
 
+        #      self.__cmd = "docker exec -it rdisc_source9 sh"
+        self.__cmd = "docker exec -it rdisc_dst ./rdisc -r"
+        self.__docker_id3 = subprocess.check_output(self.__cmd.format().split()).strip()
+        print(self.__docker_id3)
+
+        print("aaaa")
+        self.__cmd = "docker exec -it rdisc_src ./rdisc -s"
+        self.__docker_id4 = subprocess.check_output(self.__cmd.format().split()).strip()
+        print("ccc")
+        print(self.__docker_id4)
+        print("bbb")
         return True
 
 
     def kill_docker_instances(self):
         print("Killing all Docker containers")
         try:
+            # subprocess.check_output("docker container prune", shell=True)
             countainer_hashes = subprocess.check_output("docker kill $(docker ps -q)", shell=True)
             print(countainer_hashes)
         except Exception:
             print("No Docker containers were running")
+        print "Killing Docker containers"
+        #  os.system("docker kill {}".format(self.__docker_id1))
+        # os.system("docker kill {}".format(self.__docker_id2))
+        # print "Killing Docker containers"
+        # os.system("docker kill {}".format(self.__docker_id3))
+        # os.system("docker kill {}".format(self.__docker_id4))
+        # All existing SNMP Trap configurations are deleted from IDU
 
 
-Power_System = router_discovery().step_1()
+router_discovery().step_1()
 
 
