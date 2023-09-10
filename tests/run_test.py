0d1d9997701840f3b98cb10ff25ef683c34e6737
Peter Kabiri <peter.kabiri@gmail.com>
2023-09-10 17:52:38 +0200

    commit of fully working test

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,77 +1,115 @@
-import os
 import subprocess
-
+from subprocess import Popen, PIPE
+import time
 
 class router_discovery():
 
     def __init__(self):
 
 
-        self.__docker_id1 = None
-        self.__docker_id2 = None
-        self.__docker_id3 = None
-        self.__docker_id4 = None
-
-
+        self.__docker_src = None
+        self.__docker_dest = None
+        self.__docker_src_hash = None
+        self.__docker_dest_hash = None
 
         self.__cmd = None
 
-       # docker run --name rdisc_source -itd rdisc_image sh
-       # docker run --name rdisc_destination -itd rdisc_image sh
-
-
-       #  docker exec -it rdisc_source ./rdisc -r
-       #  docker exec -it rdisc_destination ./rdisc -s
 
     def step_1(self):
         self.kill_docker_instances()
- 
-        print("Starting Docker containers")
-        test = subprocess.check_output("pwd")
-        #         self.__docker_id1 = subprocess.check_output(self.__cmd.format(self.__system_vlan_interface, self.__ip1, mod_id).split()).strip()
-        self.__cmd = "docker run --name rdisc_src -itd rdisc_image sh"
-        self.__docker_id1 = subprocess.check_output(self.__cmd.format().split()).strip()
 
-        print(self.__docker_id1)
+        self.__cmd = "docker run --name rdisc_src -itd rdisc_image sh"
+        self.__docker_src = subprocess.run(self.__cmd,  capture_output=True, shell=True)
 
 
         self.__cmd = "docker run --name rdisc_dst -itd rdisc_image sh"
 
-        self.__docker_id2 = subprocess.check_output(self.__cmd.format().split()).strip()
+        self.__docker_dest = subprocess.run(self.__cmd,  capture_output=True, shell=True)
 
 
-        print(self.__docker_id2)
+        time.sleep(5)
 
-        #      self.__cmd = "docker exec -it rdisc_source9 sh"
-        self.__cmd = "docker exec -it rdisc_dst ./rdisc -r"
-        self.__docker_id3 = subprocess.check_output(self.__cmd.format().split()).strip()
-        print(self.__docker_id3)
+        self.__cmd = "docker exec rdisc_dst ./rdisc -r"
 
-        print("aaaa")
-        self.__cmd = "docker exec -it rdisc_src ./rdisc -s"
-        self.__docker_id4 = subprocess.check_output(self.__cmd.format().split()).strip()
-        print("ccc")
-        print(self.__docker_id4)
-        print("bbb")
-        return True
+        self.__docker_src_hash = subprocess.Popen([self.__cmd], shell=True, stdin=PIPE, stdout=PIPE)
+        docker_src_hash_output, docker_src_hash_output_error = self.__docker_src_hash.communicate()
 
+        self.__cmd = "docker exec rdisc_src ./rdisc -s"
+
+        self.__docker_dest_hash  = subprocess.Popen([self.__cmd], shell=True, stdin=PIPE, stdout=PIPE)
+
+        docker_dest_hash_output, docker_dest_hash_error  = self.__docker_dest_hash.communicate()
+
+        self.__docker_src_hash.kill()
+
+        self.__docker_dest_hash.kill()
+
+        state = list()
+
+        if docker_src_hash_output_error is not None:
+            print("Test failed with Error %" % docker_src_hash_output_error)
+            state.append(False)
+
+        if docker_dest_hash_error is not None:
+            print("Test failed with Error %" % docker_src_hash_output_error)
+            state.append(False)
+
+        index = 0
+        for word in docker_dest_hash_output.decode('utf-8').split('\n'):
+            if "Multicast to interface eth0" in str(word):
+                print(word)
+                state.append(True)
+                index += 1
+
+        if index == 3:
+            print("\nSolicitation message was sent three times\n")
+            state.append(True)
+        else:
+            print("\nSolicitation message was sent only %s times, not 3 times\n" % index)
+            state.append(False)
+
+        if all(state):
+            print("Test Passed. Solicitation messages were sent succesfully after Router adverstisement")
+            self.kill_docker_instances()
+            return True
+        else:
+            print("Test failed!!! Solicitation message were not correctly sent")
+            self.kill_docker_instances()
+            return False
 
     def kill_docker_instances(self):
-        print("Killing all Docker containers")
+        print("\nKilling all Docker containers")
         try:
             # subprocess.check_output("docker container prune", shell=True)
-            countainer_hashes = subprocess.check_output("docker kill $(docker ps -q)", shell=True)
-            print(countainer_hashes)
+            countainer_hashes = subprocess.run("docker kill $(docker ps -q)", shell=True, capture_output=True)
+
+            # print(countainer_hashes)
         except Exception:
             print("No Docker containers were running")
-        print "Killing Docker containers"
-        #  os.system("docker kill {}".format(self.__docker_id1))
-        # os.system("docker kill {}".format(self.__docker_id2))
-        # print "Killing Docker containers"
-        # os.system("docker kill {}".format(self.__docker_id3))
-        # os.system("docker kill {}".format(self.__docker_id4))
-        # All existing SNMP Trap configurations are deleted from IDU
 
+        try:
+            proc = subprocess.Popen(["docker container prune -f "], shell=True, stdin=PIPE, stdout=PIPE,
+                                    universal_newlines=True)
+            output, errors = proc.communicate(input="yes", timeout=15)
+            # print(output)
+            # print(errors)
+            proc.kill()
+
+        except Exception:
+            print("No Docker containers were running")
+
+
+        try:
+            proc = subprocess.Popen(["docker system prune -f"], shell=True, stdin=PIPE, stdout=PIPE,
+                                    universal_newlines=True)
+            outs, errs = proc.communicate(input="yes", timeout=15)
+            # print(output)
+            # print(errors)
+            proc.kill()
+
+
+        except Exception:
+            print("No Docker containers were running")
 
 router_discovery().step_1()
 
